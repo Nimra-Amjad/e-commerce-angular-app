079b8644494f703fcd41f482fe54b5eed9882fb8
Nimra Amjad <74602690+Nimra-Amjad@users.noreply.github.com>
2024-02-19 00:42:35 +0500

    signup-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, SellerAuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+@NgModule({
+  imports: [FormsModule]
+})
+
 export class AppComponent {
   title = 'e-commerce-app';
 }
